3ef1c2fa609bac21d86de8886f990cf7c4008ad3
Serkan Özel <serkanozel380@gmail.com>
2021-02-21 23:15:50 +0300

    do not support main and master branches auto completed

--- index.js
+++ index.js
@@ -26,131 +26,118 @@ app.get('*', (req, res, next) => {
     }
     const user = words[0];
     const repo = words[1];
-    let branch = words.length == 3 ? words[2] : 'master';
+    const branch = words.length == 3 ? words[2] : 'master';
     // console.log(words);
     try {
         // console.log(`https://api.github.com/repos/${user}/${repo}/branches/${branch}`);
-        https.get(`https://api.github.com/repos/${user}/${repo}/branches/${branch}`, {
-            headers: {
-                'User-Agent': 'srknzl/github-line-counter',
-            },
-        }, (response) => {
-            if (response.statusCode == 404) {
-                branch = 'main';
-            }
+        const fileUUID = uuid.v4();
+        // console.log('created file');
+        const file = fs.createWriteStream(`${fileUUID}.zip`);
+        try {
+            https.get(`https://codeload.github.com/${user}/${repo}/zip/${branch}`, (response) => {
+                if (response.statusCode != 200) {
+                    file.destroy();
+                    fs.unlinkSync(`${fileUUID}.zip`);
+                    return res.status(404).json({
+                        'status': 'Not found',
+                    });
+                } else {
+                    response.pipe(file);
+                }
+            })
+                .on('error', (err) => {
+                    file.destroy();
+                    fs.unlinkSync(`${fileUUID}.zip`);
+                    return res.status(500).json({
+                        'status': 'Could not download repo',
+                    });
+                });
+        } catch (error) {
+            file.destroy();
+            fs.unlinkSync(`${fileUUID}.zip`);
+            return res.status(500).json({
+                'status': 'Could not download repo',
+            });
+        }
 
-            const fileUUID = uuid.v4();
-            // console.log('created file');
-            const file = fs.createWriteStream(`${fileUUID}.zip`);
-            try {
-                https.get(`https://codeload.github.com/${user}/${repo}/zip/${branch}`, (response) => {
-                    if (response.statusCode != 200) {
-                        file.destroy();
-                        fs.unlinkSync(`${fileUUID}.zip`);
-                        return res.status(404).json({
-                            'status': 'Not found',
+        try {
+            file.on('finish', () => {
+                // console.log('finish');
+                const unzipProcess = spawn('unzip', ['-o', `${fileUUID}.zip`], {
+                    'stdio': 'ignore',
+                });
+                unzipProcess.on('exit', (code, signal) => {
+                    // console.log('unzip exit');
+                    if (code != 0) {
+                        return res.status(500).json({
+                            'status': 'Could not get code',
                         });
                     } else {
-                        response.pipe(file);
-                    }
-                })
-                    .on('error', (err) => {
-                        file.destroy();
-                        fs.unlinkSync(`${fileUUID}.zip`);
-                        return res.status(500).json({
-                            'status': 'Could not download repo',
+                        const clocProcess = spawn('node_modules/.bin/cloc', [
+                            '--quiet',
+                            '--hide-rate',
+                            '--yaml',
+                            `${repo}-${branch}`,
+                        ], {
+                            'stdio': ['ignore', 'pipe', 'pipe'],
                         });
-                    });
-            } catch (error) {
-                file.destroy();
-                fs.unlinkSync(`${fileUUID}.zip`);
-                return res.status(500).json({
-                    'status': 'Could not download repo',
+                        clocProcess.on('exit', (code, _) => {
+                            // console.log('clocProcess exit');
+                            if (code != 0) {
+                                let chunk;
+                                clocProcess.stderr.on('readable', () => {
+                                    while (null !== (chunk = clocProcess.stderr.read())) {
+                                        res.write(chunk);
+                                    }
+                                });
+                                clocProcess.stderr.on('end', () => {
+                                    try {
+                                        fs.unlinkSync(`${fileUUID}.zip`);
+                                        fs.rmdirSync(`${repo}-${branch}`, {
+                                            recursive: true,
+                                        });
+                                    } catch (error) {
+                                        // no op
+                                    }
+                                    res.status(500).end();
+                                });
+                            } else {
+                                let chunk;
+                                res.write('Powered by: ');
+                                clocProcess.stdout.on('readable', () => {
+                                    while (null !== (chunk = clocProcess.stdout.read())) {
+                                        res.write(chunk);
+                                    }
+                                });
+                                clocProcess.stdout.on('end', () => {
+                                    try {
+                                        fs.unlinkSync(`${fileUUID}.zip`);
+                                        fs.rmdirSync(`${repo}-${branch}`, {
+                                            recursive: true,
+                                        });
+                                    } catch (error) {
+                                        // no op
+                                    }
+                                    res.status(200).end();
+                                });
+                            }
+                        });
+                    }
                 });
-            }
-
-            try {
-                file.on('finish', () => {
-                    // console.log('finish');
-                    const unzipProcess = spawn('unzip', ['-o', `${fileUUID}.zip`], {
-                        'stdio': 'ignore',
-                    });
-                    unzipProcess.on('exit', (code, signal) => {
-                        // console.log('unzip exit');
-                        if (code != 0) {
-                            return res.status(500).json({
-                                'status': 'Could not get code',
-                            });
-                        } else {
-                            const clocProcess = spawn('node_modules/.bin/cloc', [
-                                '--quiet',
-                                '--hide-rate',
-                                '--yaml',
-                                `${repo}-${branch}`,
-                            ], {
-                                'stdio': ['ignore', 'pipe', 'pipe'],
-                            });
-                            clocProcess.on('exit', (code, _) => {
-                                // console.log('clocProcess exit');
-                                if (code != 0) {
-                                    let chunk;
-                                    clocProcess.stderr.on('readable', () => {
-                                        while (null !== (chunk = clocProcess.stderr.read())) {
-                                            res.write(chunk);
-                                        }
-                                    });
-                                    clocProcess.stderr.on('end', () => {
-                                        try {
-                                            fs.unlinkSync(`${fileUUID}.zip`);
-                                            fs.rmdirSync(`${repo}-${branch}`, {
-                                                recursive: true,
-                                            });
-                                        } catch (error) {
-                                            // no op
-                                        }
-                                        res.status(500).end();
-                                    });
-                                } else {
-                                    let chunk;
-                                    res.write('Powered by: ');
-                                    clocProcess.stdout.on('readable', () => {
-                                        while (null !== (chunk = clocProcess.stdout.read())) {
-                                            res.write(chunk);
-                                        }
-                                    });
-                                    clocProcess.stdout.on('end', () => {
-                                        try {
-                                            fs.unlinkSync(`${fileUUID}.zip`);
-                                            fs.rmdirSync(`${repo}-${branch}`, {
-                                                recursive: true,
-                                            });
-                                        } catch (error) {
-                                            // no op
-                                        }
-                                        res.status(200).end();
-                                    });
-                                }
-                            });
-                        }
-                    });
-                });
-            } catch (error) {
-                try {
-                    fs.unlinkSync(`${fileUUID}.zip`);
-                    fs.rmdirSync(`${repo}-${branch}`, {
-                        recursive: true,
-                    });
-                } catch (error) {
-                    // no op
-                }
-                res.status(500).json({
-                    'status': error.toString(),
-                });
-            }
-        })
-            .on('error', (err) => {
-                return res.status(500).send('Could not reach to github api.');
             });
+        } catch (error) {
+            try {
+                fs.unlinkSync(`${fileUUID}.zip`);
+                fs.rmdirSync(`${repo}-${branch}`, {
+                    recursive: true,
+                });
+            } catch (error) {
+                // no op
+            }
+            res.status(500).json({
+                'status': error.toString(),
+            });
+        }
     } catch (error) {
         return res.status(500).send('Could not reach to github api.');
     }
